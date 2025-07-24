798fdbf1d6924930917b6b7ecf00d2919c607dbe
David Schneider <davidsch@ee.ethz.ch>
2025-07-24 14:22:49 +0200

    added d-phys patch for reporting mail sender addr

--- lib/BaNG/Reporting.pm
+++ lib/BaNG/Reporting.pm
@@ -1010,7 +1010,7 @@ sub mail_report {
         );
 
         my $mail_msg = MIME::Lite->new(
-            From    => 'root@phys.ethz.ch',
+            From    => $serverconfig{report_from} || 'root',
             To      => $serverconfig{report_to},
             Type    => 'multipart/alternative',
             Subject => "Backup report of ($host-$group): $status",
